df3dc86d554ff520550fdd041a7d43f29b06c209
Erfan Sabouri <erfaansabouri@gmail.com>
2025-08-09 20:16:25 +0330

    fix

--- app/Filament/Resources/CommentResource.php
+++ app/Filament/Resources/CommentResource.php
@@ -5,10 +5,16 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\CommentResource\Pages;
 use App\Filament\Resources\CommentResource\RelationManagers;
 use App\Models\Comment;
+use App\Models\Ticket;
+use App\Models\TicketStatus;
+use App\Notifications\AdminAnsweredTicketNotification;
 use Filament\Forms;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
@@ -20,10 +26,9 @@ class CommentResource extends Resource {
 
     public static function form ( Form $form ): Form {
         return $form->schema([
-
                                  Forms\Components\Textarea::make('text')
                                                           ->required()
-                                                          ->translateLabel(),
+                                                          ->translateLabel() ,
                              ]);
     }
 
@@ -36,19 +41,42 @@ class CommentResource extends Resource {
                                    TextColumn::make('text')
                                              ->translateLabel() ,
                                    Tables\Columns\ToggleColumn::make('verified')
-                                                              ->translateLabel(),
-                                   ])
+                                                              ->translateLabel() ,
+                               ])
                      ->filters([//
                                ])
                      ->actions([
                                    Tables\Actions\EditAction::make() ,
                                    Tables\Actions\DeleteAction::make() ,
+                                   Action::make('answer_a_comment')
+                                         ->button()
+                                       ->label('پاسخ دادن')
+                                         ->outlined()
+                                         ->icon('heroicon-m-pencil-square')
+                                         ->translateLabel()
+                                         ->form([
+                                                    Textarea::make('text_of_admin')
+                                                            ->label('پاسخ ادمین')
+                                                            ->required() ,
+                                                ])
+                                         ->action(function ( Comment $comment , array $data ) {
+                                             Comment::query()
+                                                    ->create([
+                                                                 'user_id' => 2 ,
+                                                                 'text' => $data[ 'text_of_admin' ] ,
+                                                                 'commentable_id' => $comment->commentable_id ,
+                                                                 'commentable_type' => $comment->commentable_type ,
+                                                                 'parent_id' => $comment->id ,
+                                                                 'verified' => true,
+                                                             ]);
+                                         }) ,
                                ])
                      ->bulkActions([
                                        Tables\Actions\BulkActionGroup::make([
                                                                                 Tables\Actions\DeleteBulkAction::make() ,
                                                                             ]) ,
-                                   ])->defaultSort('id', 'desc');
+                                   ])
+                     ->defaultSort('id' , 'desc');
     }
 
     public static function getRelations (): array {
@@ -63,7 +91,6 @@ class CommentResource extends Resource {
             'edit' => Pages\EditComment::route('/{record}/edit') ,
         ];
     }
-
 
     public static function getLabel (): ?string {
         return __('Comment');
