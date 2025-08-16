2e7d734aa49f43d93aefb8d2d1d5c8bd418b42c7
Erfan Sabouri <erfaansabouri@gmail.com>
2025-08-16 18:34:10 +0330

    fix

--- app/Filament/Resources/AiSettingResource.php
+++ app/Filament/Resources/AiSettingResource.php
@@ -14,58 +14,47 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-class AiSettingResource extends Resource
-{
+class AiSettingResource extends Resource {
     protected static ?string $model = AiSetting::class;
-
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                         Forms\Components\Textarea::make('system_content')
-                                  ->label('پرامپت شخصی سازی')
-                                  ->required()
-                                  ->columnSpan(2) ,
-                         Forms\Components\Textarea::class::make('max_completion_tokens')
-                                                  ->label('حداکثر توکن مصرفی به ازای هر پیام')
-                                                  ->required()
-                                                  ->columnSpan(2) ,
-            ]);
+    public static function form ( Form $form ): Form {
+        return $form->schema([
+                                 Forms\Components\Textarea::make('system_content')
+                                                          ->label('پرامپت شخصی سازی')
+                                                          ->required()
+                                                          ->columnSpan(2) ,
+                                 Forms\Components\TextInput::class::make('max_completion_tokens')
+                                                                  ->label('حداکثر توکن مصرفی به ازای هر پیام')
+                                                                  ->required()
+                                                                  ->nullable()
+                                                                  ->columnSpan(2) ,
+                             ]);
     }
 
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                //
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                ]),
-            ]);
+    public static function table ( Table $table ): Table {
+        return $table->columns([//
+                               ])
+                     ->filters([//
+                               ])
+                     ->actions([
+                                   Tables\Actions\EditAction::make() ,
+                               ])
+                     ->bulkActions([
+                                       Tables\Actions\BulkActionGroup::make([]) ,
+                                   ]);
     }
 
-    public static function getRelations(): array
-    {
-        return [
-            //
+    public static function getRelations (): array {
+        return [//
         ];
     }
 
-    public static function getPages(): array
-    {
+    public static function getPages (): array {
         return [
-            'index' => Pages\ListAiSettings::route('/'),
-            'create' => Pages\CreateAiSetting::route('/create'),
-            'edit' => Pages\EditAiSetting::route('/{record}/edit'),
+            'index' => Pages\ListAiSettings::route('/') ,
+            'create' => Pages\CreateAiSetting::route('/create') ,
+            'edit' => Pages\EditAiSetting::route('/{record}/edit') ,
         ];
     }
 
